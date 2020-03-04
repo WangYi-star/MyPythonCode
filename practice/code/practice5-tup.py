a7537b4c35890a235e7e37ec09eabbda0401582b
18730728186 <1798679559@qq.com>
2020-03-04 23:56:49 +0800

    practice

---
+++ practice/code/practice5-tup.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : practice5-tup.py
+@Time : 2020/02/28 19:57:53
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+tup1=(23,45,85,64,52)
+tup2=(21,51,93,52,48)
+list=[26,45,84,55,66]
+print(tup1[2:])
+print(tup2[1:-1])
+print(tup1*2)
+print(tup1+tup2)
+print(len(tup1))
+print(max(tup1))
+print(min(tup1))
+print(sum(tup1))
+print(tuple(list))
