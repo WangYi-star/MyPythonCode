336632f1fa19439794d34bf048de4785b98579d9
18730728186 <1798679559@qq.com>
2020-03-18 22:04:10 +0800

    1.17 file-I/O

---
+++ practice/os.path.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+'''
+@File : 1.17-prac1.py
+@Time : 2020/03/18 08:29:51
+@Author : xdbcb8 
+@Version : 1.0
+@Contact : xdbcb8@qq.com
+@WebSite : www.xdbcb8.com
+'''
+
+# here put the import lib
+import os
+print(os.getcwd())
+print(os.path.basename(r'E:\myser\notepad\SQL.txt'))
+print(os.path.dirname(r'E:\myser\notepad\SQL.txt'))
+print(os.path.split(r'E:\myser\notepad\SQL.txt'))
